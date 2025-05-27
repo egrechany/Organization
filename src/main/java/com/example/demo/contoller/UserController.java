36fe5114086e22cba195a4d2041594b039fc4aca
ZhenyaGrechany <32041841+ZhenyaGrechany@users.noreply.github.com>
2025-05-27 15:58:21 +0300

    Added Cross origin

--- src/main/java/com/example/demo/contoller/UserController.java
+++ src/main/java/com/example/demo/contoller/UserController.java
@@ -2,6 +2,7 @@ package com.example.demo.contoller;
 
 import com.example.demo.repository.User;
 import com.example.demo.service.UserService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(path = "api/associates")
 public class UserController {
 
